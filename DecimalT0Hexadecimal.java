d2cc3e54caebf5d877d267846890db2872e19cd3
Jitendra Arya <114763869+Jitendr123@users.noreply.github.com>
2022-12-21 22:07:32 +0530

    Update DecimalT0Hexadecimal.java

--- DecimalT0Hexadecimal.java
+++ DecimalT0Hexadecimal.java
@@ -1,13 +1,14 @@
 public class Use1 {
 	public static void main(String arg[]) {		
      Scanner sc=new Scanner(System.in);
-		StringBuilder str=new StringBuilder();
-		int n=sc.nextInt();
-		while(n>0) {
-			int rem=n%8;
-			n/=8;
-			str.append(rem);
-		}
-		System.out.println(str.reverse().toString());
-  }
+        String s="";
+        char arr[]= {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+        int n=sc.nextInt();
+        while(n>0) {
+        	int rem=n%16;
+        	n/=16;
+        	s=arr[rem]+s;
+        }
+        System.out.println(s);	
+	}
 }
